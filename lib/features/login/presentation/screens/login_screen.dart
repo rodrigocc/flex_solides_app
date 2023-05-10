170b09d88f14fd3c64348a8ad0fd00a2d9d5aa1f
Walex Souza <walexsouza@tangerino.com.br>
2023-05-10 14:53:53 -0300

    Feat: create presentation

---
+++ lib/features/login/presentation/screens/login_screen.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.orange,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: MediaQuery.of(context).size.height / 2.74),
+            Material(
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(15),
+                topRight: Radius.circular(15),
+              ),
+              elevation: 4,
+              child: Container(
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(15),
+                    topRight: Radius.circular(15),
+                  ),
+                ),
+                height: 550,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: const [
+                      SizedBox(height: 50),
+                      TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.teal)),
+                          hintText: 'Digite seu login',
+                          prefixIcon: Icon(
+                            Icons.person,
+                            color: Colors.green,
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.teal)),
+                          hintText: 'Digite sua senha',
+                          prefixIcon: Icon(
+                            Icons.password_outlined,
+                            color: Colors.green,
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 40),
+
+                      ElevatedButton(onPressed: null, child: Text('Login'),
+                      
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
