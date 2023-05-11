7990d645079b65b4fdc32a49559f2f1e98578e37
Rodrigo Araujo <rodrigoasilvacc@gmail.com>
2023-05-11 11:38:30 -0300

    Feat : Implements a Dependency Injection Sctructure

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flex_solides_app/features/flex_solides_app.dart';
 import 'package:flutter/material.dart';
 
+import 'injection_container.dart' as di;
+
 void main() {
   runApp(const FlexSolidesApp());
+
+  di.init();
 }
